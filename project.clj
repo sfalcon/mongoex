0633cabb5f5f3d3f5360534177d2d5fd2ef489a8
Samuel Falcon <samuel.falcon.fdez@gmail.com>
2015-04-30 10:30:56 +0100

    Initial setup

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject mongoex "0.1.0-SNAPSHOT"
+  :description "An example of clojure interaction with MongoDB"
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.novemberain/monger "2.0.0"]
+                 [cheshire "5.4.0"]
+                 [compojure "1.3.3"]
+                 [ring "1.3.2"]]
+  :plugins [[lein-marginalia "0.8.0"]
+            [lein-ring "0.9.3"]]
+   :aliases {"autotest" ["midje" ":autotest"]}
+  :main ^:skip-aot mongoex.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[midje "1.6.3"]]}})
---
+++ src/mongoex/core.clj
@@ -0,0 +1,7 @@
+(ns mongoex.core
+  (:gen-class))
+
+(defn -main
+  ""
+  [& args]
+  (println "Hello, World!"))
